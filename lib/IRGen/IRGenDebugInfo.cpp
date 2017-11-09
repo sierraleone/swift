6c37bd90c570e652bb84684e3e2c663e309b48af
John McCall <rjmccall@apple.com>
2017-11-09 16:38:14 -0500

    Add a "token" type to SIL to allow dependencies to be expressed without allowing abstraction.

--- lib/IRGen/IRGenDebugInfo.cpp
+++ lib/IRGen/IRGenDebugInfo.cpp
@@ -1358,6 +1358,7 @@ private:
     case TypeKind::Module:
     case TypeKind::SILBlockStorage:
     case TypeKind::SILBox:
+    case TypeKind::SILToken:
     case TypeKind::BuiltinUnsafeValueBuffer:
 
       DEBUG(llvm::errs() << "Unhandled type: "; DbgTy.getType()->dump();
